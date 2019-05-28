800fbe486fd8e696e6f2b50b7a013be918a7bd3d
Daniel Cruz <bladoxp@gmail.com>
2019-05-28 21:49:10 +0200

    During Week2

--- SmallPrograms.py
+++ SmallPrograms.py
@@ -22,3 +22,12 @@ for w in words[:]:  # Loop over a slice copy of the entire list.
 a = ['Mary', 'had', 'a', 'little', 'lamb']
 for i in range(len(a)):
     print i, a[i]
+
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print n, 'equals', x, '*', n/x
+            break
+    else:
+        # loop fell through without finding a factor
+        print n, 'is a prime number'
