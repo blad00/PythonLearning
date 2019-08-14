475b536f13e723558a255ab90c055072dcae4b7b
dacru <dacru@psb.vib-ugent.be>
2019-08-14 16:34:30 +0200

    Del statement

--- SmallPrograms.py
+++ SmallPrograms.py
@@ -267,3 +267,26 @@ matrix = [
     [5, 6, 7, 8],
     [9, 10, 11, 12],
 ]
+#The following list comprehension will transpose rows and columns:
+[[row[i] for row in matrix] for i in range(4)]
+#less comprehen
+transposed = []
+for i in range(4):
+    transposed.append([row[i] for row in matrix])
+transposed
+
+#no comprenhen
+transposed = []
+for i in range(4):
+# the following 3 lines implement the nested listcomp
+    transposed_row = []
+    for row in matrix:
+        transposed_row.append(row[i])
+    transposed.append(transposed_row)
+
+#zip does that
+list(zip(*matrix))
+
+#5.2. The del statement
+
+
